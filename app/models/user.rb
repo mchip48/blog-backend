02e492b6316515990435e61d2ecfa5c6438447d4
Matthew Chipkin <mchipkin21@gmail.com>
2025-12-02 19:32:03 -0600

    created sessions controller an its tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password #hashes password and allows us to use password and password confirmation
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
