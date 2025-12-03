d34a8ca7a500975946e475a455445a91622f919c
Matthew Chipkin <mchipkin21@gmail.com>
2025-12-02 20:08:55 -0600

    added tests for sessions controller, successful rails test run, fixed delete route for sessions controller route

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,8 @@ Rails.application.routes.draw do
 
   #users routes
   post "/users" => "users#create"
+
+  #sessions routes
+  post "/sessions" => "sessions#create"
+  delete "/sessions" => "sessions#destroy"
 end
